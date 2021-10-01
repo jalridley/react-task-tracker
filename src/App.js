2ec8ddb89a90c20c745984908849d0019a5d8e6b
Jalynn Ridley <jallywally@gmail.com>
2021-10-01 15:58:58 +0200

    created tasks compponents and set it state in app

--- src/App.js
+++ src/App.js
@@ -6,13 +6,36 @@
 //         return <h1>class component</h1>;
 //     }
 // }
-
+import { useState } from 'react';
 import Header from './components/Header';
+import Tasks from './components/Tasks';
 
 function App() {
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: 'Doctor',
+            day: '01.12.2021 at 10am',
+            reminder: 'true',
+        },
+        {
+            id: 2,
+            text: 'coffee with babe',
+            day: '04.12.2021 at 4pm',
+            reminder: 'true',
+        },
+        {
+            id: 3,
+            text: 'flight',
+            day: '18.12.2021 at 7am',
+            reminder: 'false',
+        },
+    ]);
+
     return (
         <div className="container">
             <Header />
+            <Tasks tasks={tasks} />
         </div>
     );
 }
