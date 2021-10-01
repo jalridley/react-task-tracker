7fd76c87c8d36cc5fab1340af4c73e9251e9cefe
Jalynn Ridley <jallywally@gmail.com>
2021-10-01 17:26:16 +0200

    created delete task event

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,7 +1,17 @@
-const task = ({ task }) => {
+// npm i react-icons to get the icons then restart server
+// FaTimes is the X icon from font  awesome and it acts like a component <FaTimes />
+import { FaTimes } from 'react-icons/fa';
+
+const task = ({ task, onDelete }) => {
     return (
         <div className="task">
-            <h3>{task.text}</h3>
+            <h3>
+                {task.text}{' '}
+                <FaTimes
+                    style={{ color: 'red', cursor: 'pointer' }}
+                    onClick={() => onDelete(task.id)}
+                />
+            </h3>
             <p>{task.day}</p>
         </div>
     );
